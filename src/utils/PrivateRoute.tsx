b1ca5a5067df928ca67a0381b1c54a4bdf4b1cc1
Gisella <2382019@unai.edu>
2025-04-16 10:26:08 +0700

    first commit

---
+++ src/utils/PrivateRoute.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react";
+import { Children } from "react";
+import { useAuth } from "./AuthProvider";
+import { Navigate } from "react-router-dom";
+
+
+
+const PrivateRoute = ({ children}: { children: ReactNode }) => {
+    const { isAuthenticated } = useAuth();
+    return isAuthenticated ? children : <Navigate to="/login" />
+};
+
+export default PrivateRoute
---
+++ src/utils/PublicRoute.tsx
@@ -0,0 +1,12 @@
+import { defaultShouldDehydrateMutation } from "@tanstack/react-query";
+import { ReactNode } from "react";
+import { Children } from "react";
+import { Navigate } from "react-router-dom";
+import { useAuth } from "./AuthProvider";
+
+const PublicRoute = ({ children }: { children: ReactNode }) => {
+    const { isAuthenticated } = useAuth();
+    return isAuthenticated ? <Navigate to = "/" /> : children;
+};
+
+export default PublicRoute
